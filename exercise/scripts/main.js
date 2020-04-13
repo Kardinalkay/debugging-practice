b6822d8fe9fc71e8b668864216409bfadcb0fe10
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-04-13 21:45:26 +0000

    End game (#8)
    * Add title to app home page
    
    * Track selection of cards
    
    * If 2 cards are selected and don't match, hide them
    
    * Set a timeout to enable user see his selection before disappearance
    
    * If 2 cards are selected and don't match, hide them
    
    * Avoid repeating click on same card and address class responsible for visibility
    
    * Make error messages more fitting
    
    * Ensure cards 'flip face down' when theres a match fail
    
    * Determine logic for ending program
    
    * Introduce minimal styling and prep for user notification
    
    * Add popup notification
    
    * Hide panel when clicked and insert appropriate notification messages

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -4,6 +4,8 @@
     2b. ...and reshuffle
 3. If card has been selected already, ignore
 
+4. Check if game has ended
+
 */
 
 
@@ -12,85 +14,125 @@ window.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.card');
     let selectedCards = [];
     let matchedCards = [];
+    let $alert = document.getElementById('alert');
+
+    
+    // Click anywhere on panel to hide
+
+    $alert.onclick = () => {
+        $alert.classList.remove('show');            
+    };
+
     
     // Reveal card
     
-    function revealCard (el, selCards, card) {
+    revealCard = (el, selCards, card) => {
         el.classList.add('is-selected'); 
         selCards.push(card); 
     }
     
-    //Flip back cards
+    // Flip back cards
 
-    function flipCard(el) {
+    flipCard = (el) => {
         el.classList.remove('is-selected');
     }
+    
+    // Notify
+    
+    notify = (note="", notify="false") => {
 
+        $alert.classList.add('show');
+        $alert.textContent = (note);
+
+    }
+    
+    // Reset 
+    
+    reset = (el) => {
+        
+        selectedCards = [];
+        matchedCards = [];  
+        
+        let selected = document.querySelectorAll(el);
+
+        /*Set timeout to let user see change before flipping*/
+        setTimeout(() => { 
+
+            selected.forEach((el) => {
+                flipCard(el);
+            });
+
+        }, 1000);
+        
+        // reshuffle();
+
+    }
 
     cards.forEach(function(card) {
       
         card.addEventListener('click', function() {
-        
-        //3. 
             
-        if (card.classList.contains('is-selected')) {
-            alert ('Same selection disallowed!')
-            return;
-        }
-          
-        // 1.      
+            //4.
             
-        revealCard(card, selectedCards, card);
 
-        // 2. 
-
-        if ((selectedCards.length)%2 == 0 && selectedCards.length > 1) {
-            
-            let card1 = selectedCards[selectedCards.length - 1];
-            let card2 = selectedCards[selectedCards.length - 2];
-            
-            if (card1.innerText === card2.innerText) {
-
-                alert ("There's a match!");
-
-                matchedCards.push(card1, card2);
-                console.log(matchedCards);
-
-                card1.classList.add('is-matched');
-                card2.classList.add('is-matched');
-
-            } else {    
-
-                // Flip back cards
-                alert ("Sorry, cards do not match.");
-
-                let selected = document.querySelectorAll('.is-selected');
-
-                /*Set timeout to let user see change before flipping*/
-                setTimeout(() => { 
-
-                    selected.forEach((el) => {
-                        flipCard(el);
-                    });
-
-                }, 1000);
+            if (cards.length === (selectedCards.length + 1)) {
+                
+                let note = "Congratulations! You won."
+                
+                notify(note);
                                 
-                selectedCards = [];
-                matchedCards = [];
+                reset('.is-selected');
+                
+                
+            } else {
+                
+                //3. 
 
-                  // Reshuffle
+                if (card.classList.contains('is-selected')) {
+                    alert ('Same selection disallowed!')
+                    return;
+                }
 
-              }
+                // 1.      
 
-          }
+                revealCard(card, selectedCards, card);
 
-        // If we've matched all the cards, display a message.
-        if (matchedCards.length > cards.length) {
-            alert('You matched all the cards, nice job!');
-        }
-        
+                // 2. 
+
+                if ((selectedCards.length)%2 == 0 && selectedCards.length > 1) {
+
+                    let card1 = selectedCards[selectedCards.length - 1];
+                    let card2 = selectedCards[selectedCards.length - 2];
+
+                    if (card1.innerText === card2.innerText) {
+                        
+                        let note = "There's a match!"
+
+                        notify(note);
+                        
+                        matchedCards.push(card1, card2);
+
+                        card1.classList.add('is-matched');
+                        card2.classList.add('is-matched');
+
+                    } else {    
+
+                        // Flip back cards
+                        let note = ("Sorry, cards do not match.");
+                        
+                        notify (note);
+
+                        reset('.is-selected');
+
+                      }
+
+                  }
+
+
+            }
+
+        });
+
     });
-      
-  });
     
 });
